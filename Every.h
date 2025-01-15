2d4e16668a14352ad12eadb359c8dae618e9cf19
manhere <580122+manhere@users.noreply.github.com>
2025-01-15 09:28:27 +0800

    Update Every.h

--- Every.h
+++ Every.h
@@ -3,25 +3,26 @@
 
 class Every {
   public:
-	 enum Unit {milliseconds,seconds,minutes,hours};
-    Every(uint32_t  interval, Unit unit);
+    enum Unit {milliseconds,seconds,minutes,hours};
+    Every(uint32_t interval, Unit unit);
     bool trigger();
-	bool ready();
+    bool ready();
     void update();
+    void instantTask();
     void pauseTask();
     void resumeTask();
-	void updateEvery();
-    uint32_t  remaining(Unit unit);
+    void updateEvery();
+    uint32_t remaining(Unit unit);
     void setInterval(float interval, Unit unit);
-	uint32_t  getInterval(Unit unit);
+    uint32_t getInterval(Unit unit);
     void setRepeat(bool repeat);
-	uint32_t  percent();
-	uint32_t  mapValue(uint32_t  source,uint32_t  target);
+    uint32_t  percent();
+    uint32_t  mapValue(uint32_t  source,uint32_t  target);
     void setCallback(void (*callback)());
-    
+
   private:
-    uint32_t  _interval;
-    uint32_t  _previousTime;
+    uint32_t _interval;
+    uint32_t _previousTime;
     bool _triggered;
     bool _isReady;
     bool _paused;
